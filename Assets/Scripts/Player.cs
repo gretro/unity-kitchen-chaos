93eeb2f55cc22e7eacedb54db805014b86c1b01d
Gabriel Lemire <gabriel.lemire@gmail.com>
2023-11-14 19:13:59 -0500

    add collisions

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,3 +1,4 @@
+using Cinemachine.Utility;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -11,17 +12,75 @@ public class Player : MonoBehaviour
     [SerializeField]
     private GameInput gameInput;
 
+    private float playerRadius = 0.7f;
+    private float playerHeight = 2f;
+
     public bool IsWalking { get; private set; }
     
     private void Update()
     {
         var moveVector = gameInput.GetMovementVectorNormalized();
+        var moveDir = new Vector3(moveVector.x, 0, moveVector.y);
+
+        var moveDistance = this.moveSpeed * Time.deltaTime;
+
+        var isBlocked = Physics.CapsuleCast(
+            transform.position,
+            transform.position + Vector3.up * playerHeight,
+            playerRadius,
+            moveDir,
+            moveDistance
+        );
+
+        if (isBlocked)
+        {
+            // Cannot move towards moveDir
+ 
+            // Attempt only X movement
+            if (moveVector.Abs() != Vector2.right)
+            {
+                var moveDirX = new Vector3(moveDir.x, 0, 0).normalized;
+
+                isBlocked = Physics.CapsuleCast(
+                    transform.position,
+                    transform.position + Vector3.up * playerHeight,
+                    playerRadius,
+                    moveDirX,
+                    moveDistance
+                );
+
+                // We can move on the X axis
+                if (!isBlocked)
+                {
+                    transform.position += moveDirX * moveDistance;
+                }
+            }
+
+            if (moveVector.Abs() != Vector2.up)
+            {
+                var moveDirZ = new Vector3(0, 0, moveDir.z).normalized;
+
+                isBlocked = Physics.CapsuleCast(
+                    transform.position,
+                    transform.position + Vector3.up * playerHeight,
+                    playerRadius,
+                    moveDirZ,
+                    moveDistance
+                );
+
+                // We can move on the Y axis
+                if (!isBlocked)
+                {
+                    transform.position += moveDirZ * moveDistance;
+                }
+            }
+
+        } else
+        {
+            transform.position += moveDir * moveDistance;
+        }
 
         IsWalking = moveVector != Vector2.zero;
-
-        var moveDir = new Vector3(moveVector.x, 0, moveVector.y);
-        transform.position += moveDir * (this.moveSpeed * Time.deltaTime);
-        
         transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
     }
 }
