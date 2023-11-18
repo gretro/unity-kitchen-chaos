edf494ffd4edd59327b1a1428e2e877302db7fc2
Gabriel Lemire <gabriel.lemire@gmail.com>
2023-11-17 22:09:18 -0500

    Implement stove counter

---
+++ Assets/Scripts/Counters/StoveCounter.cs
@@ -0,0 +1,128 @@
+﻿using Assets.Scripts;
+using UnityEngine;
+
+public class StoveCounter : MonoBehaviour, IKitchenObjectHolder, IInteractable
+{
+    [SerializeField]
+    private Transform holdPoint;
+
+    [SerializeField]
+    private GameObject[] cookingElements;
+
+    [SerializeField]
+    private ProgressBarController progressBar;
+
+    private KitchenObject heldObject;
+
+    public bool CanReceiveObject(KitchenObject kitchenObject)
+    {
+        return !this.HoldsObject() && kitchenObject.TryGetComponent<Fryable>(out var _);
+    }
+
+    public KitchenObject GetHeldObject()
+    {
+        return this.heldObject;
+    }
+
+    public bool HoldsObject()
+    {
+        return heldObject != null;
+    }
+
+    public void ReceiveObject(KitchenObject kitchenObject)
+    {
+        if (!this.CanReceiveObject(kitchenObject))
+        {
+            Debug.LogError("Cannot receive object");
+        }
+
+        this.heldObject = kitchenObject;
+        this.heldObject.transform.SetParent(this.holdPoint);
+        this.heldObject.transform.localPosition = Vector3.zero;
+
+        var heldFryableObject = this.heldObject.GetComponent<Fryable>();
+        heldFryableObject.StartCooking();
+        heldFryableObject.FryableUpdated += this.OnFryableUpdated;
+
+        this.EnableCookingElements();
+
+        if (heldFryableObject.State != Fryable.FryState.Burned)
+        {
+            this.progressBar.Show();
+        }
+    }
+
+    private void OnFryableUpdated(object sender, Fryable.FryableUpdatedEventArgs e)
+    {
+        if (e.State == Fryable.FryState.Burned)
+        {
+            this.progressBar.Reset();
+            this.progressBar.Hide();
+
+            var fryable = sender as Fryable;
+            fryable.FryableUpdated -= this.OnFryableUpdated;
+        }
+
+        if (e.MaxProgress == 0f)
+        {
+            this.progressBar.UpdateProgress(1f, 1f);
+        }
+        else
+        {
+            this.progressBar.UpdateProgress(e.CurrentProgress, e.MaxProgress);
+        }
+    }
+
+    private void EnableCookingElements()
+    {
+        foreach (var cookingElement in this.cookingElements)
+        {
+            cookingElement.SetActive(true);
+        }
+    }
+
+    private void DisableCookingElements()
+    {
+        foreach (var cookingElement in this.cookingElements)
+        {
+            cookingElement.SetActive(false);
+        }
+    }
+
+    public void Interact(Player source)
+    {
+        if (this.HoldsObject())
+        {
+            if (source.CanReceiveObject(this.heldObject))
+            {
+                source.ReceiveObject(this.heldObject);
+                this.DisableCookingElements();
+
+                var fryable = this.heldObject.GetComponent<Fryable>();
+                fryable.StopCooking();
+                fryable.FryableUpdated -= this.OnFryableUpdated;
+                this.heldObject = null;
+
+                this.progressBar.Reset();
+                this.progressBar.Hide();
+            }
+            else
+            {
+                Debug.Log("Player cannot receive object");
+            }
+        }
+        else
+        {
+            if (source.HoldsObject())
+            {
+                source.TransferObject(this);
+            }
+            else
+            {
+                Debug.Log("Player has no object for the Stove counter");
+            }
+        }
+    }
+
+    public void AlternateInteract(Player source) { }
+}
---
+++ Assets/Scripts/KitchenObjects/Fryable.cs
@@ -0,0 +1,126 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fryable : MonoBehaviour
+{
+    public event EventHandler<FryableUpdatedEventArgs> FryableUpdated;
+
+    public class FryableUpdatedEventArgs : EventArgs
+    {
+        public FryState State { get; set; }
+
+        public float CurrentProgress { get; set; }
+        public float MaxProgress { get; set; }
+    }
+
+    public enum FryState
+    {
+        Raw,
+        Cooked,
+        Burned
+    };
+
+    private static readonly IReadOnlyDictionary<FryState, float> progressTimes = new Dictionary<FryState, float>()
+    {
+        { FryState.Raw, 5f },
+        { FryState.Cooked, 7f }
+    };
+
+    [SerializeField]
+    private Transform raw;
+
+    [SerializeField]
+    private Transform cooked;
+
+    [SerializeField]
+    private Transform burned;
+
+    private float timeCooked = 0f;
+
+    private bool isCooking = false;
+
+    private FryState state = FryState.Raw;
+    public FryState State
+    {
+        get
+        {
+            return state;
+        }
+        private set
+        {
+            state = value;
+
+            switch (state)
+            {
+                case FryState.Raw:
+                    raw.gameObject.SetActive(true);
+                    cooked.gameObject.SetActive(false);
+                    burned.gameObject.SetActive(false);
+                    break;
+                case FryState.Cooked:
+                    raw.gameObject.SetActive(false);
+                    cooked.gameObject.SetActive(true);
+                    burned.gameObject.SetActive(false);
+                    break;
+                case FryState.Burned:
+                    raw.gameObject.SetActive(false);
+                    cooked.gameObject.SetActive(false);
+                    burned.gameObject.SetActive(true);
+                    break;
+            }
+        }
+    }
+
+    public void StartCooking()
+    {
+        isCooking = true;
+    }
+
+    public void StopCooking()
+    {
+        isCooking = false;
+    }
+
+    public float GetCookingProgress()
+    {
+        if (State == FryState.Burned)
+        {
+            return 1f;
+        }
+
+        var maxTime = progressTimes[State];
+        return this.timeCooked / maxTime;
+    }
+
+    private void Update()
+    {
+        if (!isCooking || State == FryState.Burned)
+        {
+            return;
+        }
+
+        var maxTime = progressTimes[State];
+        timeCooked += Time.deltaTime;
+
+        if (timeCooked > maxTime)
+        {
+            // We progress to the next state
+            State = State switch
+            {
+                FryState.Raw => FryState.Cooked,
+                FryState.Cooked => FryState.Burned,
+                _ => FryState.Burned,
+            };
+
+            timeCooked = 0f;
+        }
+
+        FryableUpdated?.Invoke(this, new FryableUpdatedEventArgs()
+        {
+            State = State,
+            CurrentProgress = timeCooked,
+            MaxProgress = maxTime
+        });
+    }
+}
